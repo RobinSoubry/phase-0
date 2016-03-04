20cad4db77ff188c7b854d1aa079d30bfda9d394
Robin Soubry <robin.soubry@gmail.com>
2016-03-03 16:45:55 -0800

    Integrate documents Sibel Ergener from pairing

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,14 @@
+# Simple Substrings
+
+# I worked on this challenge with: Sibel Ergener.
+
+
+# Your Solution Below
+
+def welcome (address)
+	if address.upcase.include? " CA "
+		p "Welcome to California"
+	else
+		p "You should move to California"
+	end
+end
