9fca313ad44e0adafb172b5d0eff3db3ee7f5167
Robin Soubry <robin.soubry@gmail.com>
2016-03-11 16:40:53 -0800

    Complete refactor person 1 release

--- week-5/group-research-methods/methods_specs.rb
+++ week-5/group-research-methods/methods_specs.rb
@@ -1,18 +1,18 @@
 require_relative "my_solution"
 
-# # PERSON 1: FIND MATCHING DATA
-# describe 'PERSON 1: my_array_finding_method' do
-#   let(:i_want_pets) {["I", "want", 3, "pets", "but", "only", "have", 2 ]}
+# PERSON 1: FIND MATCHING DATA
+describe 'PERSON 1: my_array_finding_method' do
+  let(:i_want_pets) {["I", "want", 3, "pets", "but", "only", "have", 2 ]}
 
-#   it "selects all words with a 't' in the original order" do
-#     expect(my_array_finding_method(i_want_pets, "t")).to eq ["want","pets","but"]
-#   end
+  it "selects all words with a 't' in the original order" do
+    expect(my_array_finding_method(i_want_pets, "t")).to eq ["want","pets","but"]
+  end
 
-#   it "selects all words with an 'a' in the original order" do
-#     expect(my_array_finding_method(i_want_pets, "a")).to eq ["want","have"]
-#   end
+  it "selects all words with an 'a' in the original order" do
+    expect(my_array_finding_method(i_want_pets, "a")).to eq ["want","have"]
+  end
 
-# end
+end
 
 # describe 'PERSON 1: my_hash_finding_method' do
 
