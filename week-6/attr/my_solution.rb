8d6f0cf30f19ea5f61b3a43645d3129426146a03
Robin Soubry <robin.soubry@gmail.com>
2016-03-16 16:50:34 -0700

    Add complete solution + pseudo code + reflection

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -2,13 +2,17 @@
 
 # I worked on this challenge by myself
 
-# I spent [#] hours on this challenge.
+# I spent [2] hours on this challenge.
 
 # Pseudocode
 
-# Input:
-# Output:
+# Input: The input is limited to the creation of a new object "greet" in the Greetings class, and afterwards calling the method .hello on the object greet.
+# Output: The output is a string that sources a keyword (the name of the student) from another class.
 # Steps:
+	# Step 1: Create a class 'NameData' that initializes with an instance variable @name equal to "Robin Soubry" 
+	# Step 2: Create an attr_reader for the instance variable @name, within the class NameData 
+	# Step 3: Create a class 'Greetings' that initializes a new object for class 'NameData' for a new instance variable
+	# Step 4: Puts a scentence to the screen, that is composed of a string with the name of the student that was initialized in the NameData class.
 
 class NameData
 	attr_reader :name
@@ -18,21 +22,15 @@ class NameData
 	end
 end
 
-
 class Greetings
-	attr_reader :name
-	
 	def initialize
-		NameData.new
+		@student = NameData.new
 	end
 
 	def hello
-		puts "Hello #{@name}! How wonderful to see you today."
+		puts "Hello #{@student.name}! How wonderful to see you today."
 	end
 end
-
-test = NameData.new
-puts test.name
 
 greet = Greetings.new
 greet.hello
@@ -91,17 +89,35 @@ Is this code simpler than the last?
 # RELEASE 6 #
 =============
 What is a reader method?
-
+	A reader method returns a value or state outside of the class it was created in, but it does not change the value.
+	The reader method can be called by attr_reader :symbol_name.
+		The :symbol_name must correspond with an instance variable that was created in the class.
+	The reader method allows to "read" in values without creating a dedicated method for returning that value to the console.
+	You can call the reader method by adding it as a method to the object.
+		example: object_name.symbol_name
 
 What is a writer method?
-
+	A writer method allows to change a value or state outside of the class it was created in, but it does not read the value.
+	The reader method can be called by attr_reader :symbol_name.
+	The :symbol_name must correspond with an instance variable that was created in the class.
+	Similar to the reader method, you don't have to create a specific class to overwrite te value.
+		You can simply call it by "object_name.symbol_name = new_value"
 
 What do the attr methods do for you?
-
+	The attr methods read and/or write the values for specific instance variables by calling on a symbol.
+	This is more efficient creating a specific method to return the value or to overwrite the value.
 
 Should you always use an accessor to cover your bases? Why or why not?
-
+	The attr_accessor both reads and writes the values of an instance variable. This is very powerful, but potentially dangerous:
+		1. You don't want to expose the application to potential securuty threats more than necessary.
+		2. Debugging with both read/write may be tricky
+		3. You want to be careful to not give other methods or classes too much access to the inner workings of others.
 
 What is confusing to you about these methods?
-	
+	The examples given were very clear and straightforward.
+	When I tried to solve release 5, with the challenge on greeting across two differnet classes, I had it harder to understand:
+		* Which name of the instance variable to use as a attr_reader.
+			For example, I tried to puts #{:name}, thinking it would work
+		* When to create a new object and how to combine it with the attr_reader ":name"
+
 =end
