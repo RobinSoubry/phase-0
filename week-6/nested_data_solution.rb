dc3344db69e7d7fae11d85922c8efa788c7a390e
Robin Soubry <robin.soubry@gmail.com>
2016-03-17 09:41:20 -0700

    Add reflection

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -64,7 +64,7 @@ new_array = startup_names.map do |first_level|
 end
 p new_array
 
-# Refactoring
+# Refactoring Bonus
 
 new_startup_names = startup_names.map do |first_level| 
  if first_level.kind_of?(Array)
@@ -80,3 +80,24 @@ new_startup_names = startup_names.map do |first_level|
  end
 end
 p new_startup_names
+
+# REFLECTION
+=begin
+What are some general rules you can apply to nested arrays?
+  Nested arrays have a level of depth that can be expressed by a number of degrees.
+  When you want to access an element inside a nested array, you can 'chain' the number indices together.
+    The number of chained indices should equal the number of levels that you want to dig into in the array.
+  For example array[0][2][0] => This notation aims to access a the first element of a third-level array.
+
+What are some ways you can iterate over nested arrays?
+  To iterate over an array, you could use .each and .map.
+  However, to iterate over a nested array, you want to iterate again over the elements that are also arrays.
+    * You can identify whether an array element is an array by using the .kind_of?(Array) method in an if statement.
+      * If the statement is true (first-level element = array), you are can iterate over the 2nd-level array with another .each or .map method on the first-level element
+      * If the statement is not true (first-level element != array), you are can execute a code to the first-level element.
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+  We used methods that I was already familiar with, but combined them in a different way.
+  For example, we used the .map method in nested iterations. to access and modify the individual elements in the bonus release.
+  Another helpful method was: .kind_of?()
+=end
