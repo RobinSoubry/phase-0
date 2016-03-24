31fd370c62d09691698e83cba67943789e5c3674
Robin Soubry <robin.soubry@gmail.com>
2016-03-24 13:14:17 -0700

    Add initial commit

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,33 @@
+ // JavaScript Olympics
+
+// I paired with: Bill Deng on this challenge.
+
+// This challenge took us [#] hours.
+
+
+// Warm Up
+
+
+
+
+// Bulk Up
+
+
+
+
+// Jumble your words
+
+
+
+
+// 2,4,6,8
+
+
+
+
+// "We built this city"
+
+
+
+
+// Reflection
