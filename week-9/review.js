85ba25c72f4e1693b0f6576bade38eb95d77ca6a
Robin Soubry <robin.soubry@gmail.com>
2016-04-09 13:45:28 +0200

    Add challenge 9.2 with reflection

--- week-9/review.js
+++ week-9/review.js
@@ -1,103 +1,113 @@
+// JavaScript review of challenge 6.7: Validate Credit Card (medium long)
+
 /*
 USER STORIES
 ============
-* As a user, I want to create a new grocery list.
-* As a user I want to be able add item(s) to my grocery list and indicate the desired quantity.
-* As a user I want to be able to remove item(s) from my grocery list.
-* As a user I want to be able to update the quantities of items on my grocery list.
-* As a user I want to be able to print my grocery list in a good looking way.
+* As a user I want to input a 16-digit number and verify whether it is a valid credit card number.
 
 PSEUDO-CODE
 ============
-The grocery list will be an object that contains different funtions (new, add, update, delete and print).
-
-New grocery-list
-----------------
-Input: string of items separated by spaces (example: "carrots apples cereal pizza")
-	CALL a function 'create_list' that triggers the creation of a new grocery list.
-	This function accepts an argument for parameter 'grocery_string', which accepts a string of different items.
-Output:
-	An object with the different items on the grocery list as a parameter and a value equal to 1
-Steps:
-	Step 1: Define a function 'create_list'
-	Step 2: Split the input string 'grocery_string' on white-spaces and convert each element to a new object parameter
-
-Add item(s)
------------
-Input: new item to add to the list and an optional quantity
-Output: A modified object with the new item (and quantity added) to the object as new parameter/value pair.
-Steps:
-
+*Input: Integer with 16 digits
+* Output: Boolean (true/false) based on whether the creditcard number is valid or not
+* Steps:
+  * Step 1: Initialize the class with a parameter and define number as an instance variable.
+    * Raise argument error if the argument does not equal 16 digits
+  * Step 2: Change object type from integer (via string) to an array with individual digits (convert back to digit from string).
+  * Step 3: Create a method to double the value of every other integer , starting with index zero in the array
+  * Step 4: Create a method to split double digit integers to single digit array elements
+  * Step 5: Iterate through array and add the value of each element to a counter
+  * Step 6: Create a bolean operator: IF Divisive by 10, return true, ELSE return False
 */
 
+// INITIAL SOLUTION
+// ================
+// var checkCard = function(number){
+//   if (isNaN(number)) {
+//     console.log("The input must be a number");
+//   }
+//   else if (number.toString().length != 16){
+//     console.log('The input number must have 16 digits');
+//   }
+//   else {
+//     var num_array = (""+number).split("");
+//     for (var i = 0; i < num_array.length; i++){
+//       if (i %2 == 0){
+//         num_array[i] *= 2;
+//       } 
+//     }
+//     var string_array = num_array.join("");
+//     var final_array = string_array.split("");
+//     var total = 0;
+//     for (var j = 0; j < final_array.length; j++){
+//       total += parseInt(final_array[j]);
+//     }
+//     if (total % 10 == 0){
+//       console.log("true");
+//       return true;
+//     }
+//     else{
+//       console.log("false");
+//       return false;
+//     }  
+//   }
+// }
 
+// REFACTORED SOLUTION
+// ===================
+var checkCard = function(number){
+  if (isNaN(number)) {
+    throw new TypeError('The input must be a number');
+  }
+  else if (number.toString().length != 16){
+    throw new TypeError('The input number must have 16 digits');
+  }
+  else {
+    var card_array = (""+number).split("");
+    for (var i = 0; i < card_array.length; i++){
+      if (i %2 == 0){
+        card_array[i] *= 2;
+      } 
+    }
+    var final_array = card_array.join("").split("");
+    var total = 0;
+    for (var j = 0; j < final_array.length; j++){
+      total += parseInt(final_array[j]);
+    }
+    if (total % 10 == 0){
+      return true;
+    }
+    else{
+      return false;
+    }  
+  }
+}
 
+checkCard("A");
+checkCard(11111111111111112);
+checkCard(1);
+checkCard(4408041234567901);
+checkCard(4408041234567991);
+
+// REFLECTION
+// ==========
 /*
+What concepts did you solidify in working on this challenge? 
+  I got a more solid understanding of:
+    * JavaScript syntax in general
+    * Error handling in JavaScript (client-side validations are useful to reduce server-requests)
+    * Type handling and conversions (conversion to integer vs. string etc.)
+    * Iterations over array elements  
 
-# steps:
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: Add new item key/value pairs to the hash
-# output: Modified hash, with the added key/value
+What was the most difficult part of this challenge?
+  I find it relatively hard to 'translate' code from one Ruby to JavaScript as it limits my thinking to ruby-methods that not necessarily exist in JavaScript.
+  It seems easier to understand the logic of the ruby code and build something from scratch from the JavaScript toolkit.
 
-# Method to remove an item from the list
-# input: Key of the item that needs to be removed
-# steps: Call delete method for the specific key
-# output: Modified hash, with the deleted key removed
+Did you solve the problem in a new way this time?
+  Yes and no: I recycled the logic from the previous code, but adapted the structure quite a bit to JavaScript.
+  I also experimented with error handling in JavaScript (as it's not very strict on arguments):
+    I tried working with try{…} catch{…} and final {} but couldn't get it working well, so I used if/else logic to throw specific errors on wrong input.
 
-# Method to update the quantity of an item
-# input: Key for which item we wnt to update + new value
-# steps: Reset the value for the key.
-# output: Modified hash, with the value updated for the specific key
-
-# Method to print a list and make it look pretty
-# input: Hash with items and quantities
-# steps:
- # Iterate the hash with a formatted text.
- # Inside the iteration, the keys and values will be converted to strings and wrapped in nice structure.
-# output: Values printed on screen.
-
-
-
-
-
-# REFACTORED CODE
-def create_list (grocery_string)
- items = grocery_string.split()
- list = Hash.new (1)
- items.each {|item| list[item] = 1}
- return list
-end
-
-def add_item(list, item_name, quantity=1)
-  list[item_name] = quantity
-  return list
-end
-
-def remove_item(list, item_name)
-  list.delete(item_name)
-  return list
-end
-
-def update_item(list, item_name, quantity)
-  list[item_name] = quantity
-  return list
-end
-
-def print_list(list)
-  puts "My Grocery list"
-  list.each do |k, v|
-    key = k.capitalize
-    puts "#{key} : #{v}"
-  end
-end
-
-# DEMO DATA
-list = create_list("carrots apples cereal pizza")
-add_item(list, "Tomatoes")
-remove_item(list, "pizza")
-update_item(list, "apples", 6)
-print_list(list)
-
+Was your pseudocode different from the Ruby version? What was the same and what was different?
+  The pseudo-code is the same as it is supposed to be language-independant.
 */
 
